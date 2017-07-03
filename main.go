eb97b46e40dc4013e8de668301879874c636592a
Zixia Zhen <zixiazhen@Zixias-MacBook-Pro.local>
2017-07-02 23:48:45 -0700

    chnage onject name

--- main.go
+++ main.go
@@ -7,21 +7,9 @@ import (
 )
 
 func main() {
-	service := &resource.ProductResource{}
-	service.Initialize("http://13.59.145.88:8125", "test01")
+	service := &resource.RecordResource{}
+	service.Initialize("13.59.145.88:8125", "test01")
 	service.Register()
+	log.Println("Starting Http Server!")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-/*
-
-func main() {
-	ws := new(restful.WebService)
-	ws.Route(ws.GET("/hello").To(hello))
-	restful.Add(ws)
-	http.ListenAndServe(":8080", nil)
-}
-
-func hello(req *restful.Request, resp *restful.Response) {
-	io.WriteString(resp, "world")
-}*/
